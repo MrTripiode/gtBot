7e0b71f9086d4845fb5b16f1ea8158db9c4b1c61
Esteban Lhote <esteban.lhote@outlook.fr>
2019-01-21 10:26:45 +0100

    Initial commit

---
+++ assets/ui-admin/public/precache-manifest.7845e3cfb0bb2ec51fe031f68f0585df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a44bde7ca37765dbbc292a22788bd70",
+    "url": "/admin/static/media/nobg_white.5a44bde7.png"
+  },
+  {
+    "revision": "eb3326647359f6139ec1ade72a9bad39",
+    "url": "/admin/static/media/loading-circle.eb332664.svg"
+  },
+  {
+    "revision": "ac0a474b485b28f83fb516e86a5ada49",
+    "url": "/admin/static/media/botpress.ac0a474b.svg"
+  },
+  {
+    "revision": "3f82f5bf8566dc34c9b5",
+    "url": "/admin/static/js/runtime~main.3f82f5bf.js"
+  },
+  {
+    "revision": "3b4e08b00949a0b7ec88",
+    "url": "/admin/static/js/main.3b4e08b0.chunk.js"
+  },
+  {
+    "revision": "a388e35f3edcb70416a0",
+    "url": "/admin/static/js/1.a388e35f.chunk.js"
+  },
+  {
+    "revision": "3b4e08b00949a0b7ec88",
+    "url": "/admin/static/css/main.f7f1baba.chunk.css"
+  },
+  {
+    "revision": "a388e35f3edcb70416a0",
+    "url": "/admin/static/css/1.09d90a59.chunk.css"
+  },
+  {
+    "revision": "ca6d2520ce16e019c2dc8e4554b68af6",
+    "url": "/admin/index.html"
+  }
+];
